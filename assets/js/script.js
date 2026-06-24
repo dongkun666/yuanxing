8bf3a418eb3c1085307344d08e74b2e4de5f6f57
dongkun666 <420811808@qq.com>
2026-06-24 09:35:32 +0000

    feat: 查看代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,8 @@
             'orders': 'orders.html',
             'member-center': 'member-center.html',
             'account-settings': 'account-settings.html',
-            'archive': 'archive.html'
+            'archive': 'archive.html',
+            'contract-list': 'contract-list.html'
         };
 
         // 动态加载视图
